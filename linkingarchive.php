6f4a5b82b37de3be0b66da2ca94e5e49a84dced3
Jinglero <jinglero@gmail.com>
2015-07-28 08:49:07 -0300

    Restart Pluging from scratch

---
+++ linkingarchive.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Linking Archive
+Plugin URI: https://github.com/Jinglero/wp-linking-archive
+Description: Pluging de Wordpress para que dos wordpress sirvan de un solo sitio
+Version: 0.0.1
+Author: Pedro García
+Author URI: http://www.jinglero.com
+License: GLP2
+
+	Copyright 2015 Pedro García  (email : jinglero@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function linking_archive(){
+	echo "hola";
+	die();
+}
+
+
+?>
